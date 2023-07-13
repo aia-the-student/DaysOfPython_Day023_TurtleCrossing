1ba03fce4827aaf9e9b0f1760073154ec7ba7119
AK20202 <annakodenko@ngs.ru>
2023-07-13 14:35:30 -0400

    Player is created and can run through the screen

--- main.py
+++ main.py
@@ -4,11 +4,24 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+SLEEP_TIME = 0.1
+SLEEP_DECREASE = 0.9
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+
+screen.listen()
+screen.onkey(key='Up', fun=player.move)
+
 game_is_on = True
+
+
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(SLEEP_TIME)
     screen.update()
+
+    if player.is_crossed():
+        SLEEP_TIME *= SLEEP_DECREASE
